56e1a3378661d0838ba5c36991fc4c7c6e283f82
aimanparvaiz <aimanparavaiz@gmail.com>
2013-12-12 23:06:29 -0800

    Adding size, ownership and mode functions

--- basefile.py
+++ basefile.py
@@ -1,5 +1,5 @@
 
-import os
+import os, pwd, grp
 
 class Basefile(object):
 	"""
@@ -9,6 +9,38 @@ class Basefile(object):
 
 	def __init__(self):
 		print 'dont know what to initialize :)'
+
+	def get_mode(l_file):
+		perms="-"
+		link=""
+		mode = oct(stat.S_IMODE(os.stat(l_file).st_mode))
+
+		if stat.S_ISLINK(mode):
+			perms = "l"
+			link = os.readlink(l_file)
+			#f not os.path.exists(l_file):
+		elif stat.S_ISDIR(mode):
+			perms = "d"
+		for who in "USR", "GRP", "OTH" :
+			for what in "R", "W", "X":
+				if mode & getattr(stat, "S_I"+what+who):
+					perms=perms_what.lower()
+				else:
+					perms=perms+"-"
+		return (perms, link)		
+
+	def get_file_owners(l_file):
+		uid = os.stat(l_file).st_uid
+		gid = os.stat(l_file).st_gid
+		
+		user = pwd.getpwuid(uid)[0]
+		group = grp.getgrgid(gid)[0]
+		
+		return (user, group)
+
+	def get_file_size(l_file):
+		size = stat.ST_SIZE(l_file)
+		return size
 
 	# Option -a
 	def a(self, dir_path=None):
@@ -25,16 +57,17 @@ class Basefile(object):
 			dir_path = os.getcwd()
 		l_files = os.listdir(dir_path)
 		l_files = [ files for files in l_files if files[0] != '.' ]
-		return l_files
+		for l_file in l_files:
+			perms, link = get_mode(l_file)
+			user, grp = get_file_owners(l_file)
+			size = get_file_size(l_file)
+		
 
 class just_file(Basefile):
 
 	def stdout(self):
-		a_files = self.a()
-		#l_files = self.l()
-		for a_file in a_files:
-			print a_file
-			
+		l_files = self.l()
+					
 if __name__ == '__main__':
 	obj1 = just_file()
 	obj1.stdout()
