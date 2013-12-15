033a9ae3a51058c3682d5eae7aad6dc9430dad38
aimanparvaiz <aimanparavaiz@gmail.com>
2013-12-14 19:19:59 -0800

    There is no way in python to get '.' and '..' in the ls -a option , so manually appending it to the file list

--- basefile.py
+++ basefile.py
@@ -1,5 +1,7 @@
 
-import os, pwd, grp, stat
+from __future__ import division
+import os, pwd, grp
+import stat, time, glob
 
 class Basefile(object):
 	"""
@@ -10,10 +12,25 @@ class Basefile(object):
 	def __init__(self):
 		print 'dont know what to initialize :)'
 
+	def get_links_to_inode(self, l_file):
+		return os.stat(l_file).st_nlink
+	
+	def get_last_modification_time(self, l_file):
+		t_sec = os.stat(l_file).st_mtime
+		# Change time in s since epoch to struct_time
+
+		# Change struct_time to string given by format arg
+		return time.strftime("%b %d %H:%M", time.gmtime(t_sec))
+
+
+
+
 	def get_mode(self, l_file):
 		perms="-"
 		link=""
-		mode = int(oct(stat.S_IMODE(os.stat(l_file).st_mode)))
+		#mode = int(oct(stat.S_IMODE(os.stat(l_file).st_mode)))
+		mode = os.stat(l_file).st_mode
+		
 		if stat.S_ISLNK(mode):
 			perms = "l"
 			link = os.readlink(l_file)
@@ -47,27 +64,36 @@ class Basefile(object):
 		if not dir_path:
 			dir_path = os.getcwd()
 		# Everything in the present dir is in files now	
-		a_files = os.listdir(dir_path)
-		return a_files
+		a_files = [os.curdir, os.pardir] + os.listdir(dir_path)
+		
+
+
 
 	# Option -l
 	def l(self, dir_path=None):
+		total_size = 0
 		if not dir_path:
 			dir_path = os.getcwd()
 		l_files = os.listdir(dir_path)
 		l_files = [ files for files in l_files if files[0] != '.' ]
-
+		
 		for l_file in l_files:
 			perms, link = self.get_mode(l_file)
 			user, grp = self.get_file_owners(l_file)
 			size = self.get_file_size(l_file)
-			print perms, user, grp, size, l_file
+			total_size = total_size+size
+			links = self.get_links_to_inode(l_file)
+			mtime = self.get_last_modification_time(l_file)
+			return (perms, links, user, grp, size, mtime, l_file)
+		
 		
 
 class just_file(Basefile):
 
 	def stdout(self):
-		l_files = self.l()
+		#perms, links, user, grp, size, mtime, l_file = self.l()
+		#print perms, links, user, grp, size, mtime, l_file 
+		self.a()
 					
 if __name__ == '__main__':
 	obj1 = just_file()
